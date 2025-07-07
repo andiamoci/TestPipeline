4729a23c31ae1f3f2abbb87a513f8d42c09c0376
Karaosman-Geiler <aysan.karaosman@siemens.com>
2025-07-07 16:05:02 +0200

    initial file

---
+++ TestPipeline_LWin_RLinux.ps1
@@ -0,0 +1,13 @@
+﻿# Define remote variables
+$remoteUser = "tester"
+$remoteHost = "127.157.52.10"
+$remoteFile = "/home/tester/hello.txt"
+$remotePass = "Siemens.123"
+$hostKey = "ssh-ed25519 255 SHA256:hXGKLMItTh2W2e8E3u+YnNFW+nPPOQe1aCnNzbEGcyw"
+
+# The command to run remotely
+$remoteCommand = "echo 'Hello World' > $remoteFile"
+
+# Execute via SSH
+plink.exe -batch -ssh "$remoteUser@$remoteHost" -pw "$remotePass" -hostkey "$hostKey" "$remoteCommand"
+
